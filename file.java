501bf2fbcc43edc23b7112c18d6f7a4632700256
Sasivarna <sasireddy5678@gmail.com>
2024-08-06 17:47:38 +0530

    added java file

---
+++ file.java
@@ -0,0 +1,13 @@
+package in.sasivarna
+ int i=10;
+String str="Hello World");
+public class Demo{
+ public Demo(){
+System.out.println("Demo::Constructor);
+}
+public static main method(String[] args){
+ Demo d=new Demo();
+System.out.println(str);
+}
+}
+
